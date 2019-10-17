21def118ed35949b8e85a222e1b0e5423fc5f83d
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-10-17 16:09:28 -0400

    form is built and styled

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import './FormCSS.css';
 
 const Form = () => {
 
     return (
-        <h1>Title from Form</h1>
+        <div className='wrapper'>
+            <form>
+                <label htmlFor='name'>Name</label>
+                <input 
+                id='name'
+                type='input'
+                name='name'
+                placeholder='Full Name' />
+
+                <label htmlFor='email'>Email</label>
+                <input
+                id='email'
+                type='email'
+                name='email'
+                placeholder='email' />
+
+                <label htmlFor='password'>Password</label>
+                <input
+                id='password'
+                type='password'
+                name='password'
+                placeholder='password' />
+
+                <label htmlFor='tos'>Terms of Service</label>
+                <input
+                id='tos'
+                type='checkbox'
+                name='tos'
+                placeholder='tos' />
+
+                <button>Submit</button>
+            </form>
+        </div>
     );
 }
 export default Form;
